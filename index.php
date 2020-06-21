320d49165c537872d337f41b6752e2b2cb54d094
Tusar Haque <tusarhaque12@gmail.com>
2020-06-21 16:52:17 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<!-- Main Part Start -->
+<div class="container">
+  <div class="container-main">
+  <div class="logo-container">
+    <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="">
+  </div>
+  <div class="search-container">
+    <input class="searchbox" type="text" name="term">
+    
+      <input type="submit" name="submit-btn" value="Search" >
+    
+</div>
+</div>
+<div class="clear"></div>
+</div>
+<!-- Main-part End -->
+
+
+  
+</body>
+</html>
